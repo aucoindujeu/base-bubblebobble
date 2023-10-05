ad6bb048bf256a2f302a29621311066452e2e35a
Régis Behmo <regis@behmo.com>
2023-10-05 19:48:34 +0200

    joueur passe de bas en haut

--- main.lua
+++ main.lua
@@ -1,85 +1,132 @@
+GRAVITE = 9.81
+VENT = 0
+TAILLE_BLOC = 50
+JOUEUR_TAILLEX = 50
+JOUEUR_TAILLEY = 50
+JOUEUR_VITESSE = 300
+JOUEUR_VITESSE_SAUT = -300
+JOUEUR_VITESSE_CHUTE_MAX = 600
+JOUEUR_HITBOX = nil
+JOUEUR_SURLESOL = false
+JOUEUR = nil
+NIVEAU = {
+    blocs = {},
+    tailleY = 0
+}
+IMAGES = {
+    joueur =  love.graphics.newImage("images/joueur.png"),
+    bloc = love.graphics.newImage("images/bloc.png")
+}
+MONDE = nil
+
 function love.load()
+    -- fenêtre
     local longueurFenetre, hauteurFenetre = love.window.getDesktopDimensions()
     love.window.setMode(longueurFenetre, hauteurFenetre)
 
-    GRAVITE = 600
-    VENT = 0
-    monde = love.physics.newWorld(VENT, GRAVITE)
-    -- monde:setCallbacks("contact", "finContact")
+    -- monde et gravité
+    love.physics.setMeter(64) -- 1 mètre = 64 pixels
+    MONDE = love.physics.newWorld(VENT * 64, GRAVITE * 64)
 
-    joueur = {
-        debut_x = 0,
-        debut_y = 0,
-        longueur = 50,
-        hauteur = 100,
-        vitesse = 500,
-        forceSaut = -250,
-        image = love.graphics.newImage("images/rien.png"),
-        hitbox = nil,
-        surLeSol = false
-    }
-    joueur.hitbox = creerHitbox(joueur.debut_x, joueur.debut_y, joueur.longueur, joueur.hauteur, "dynamic")
-    joueur.hitbox.corps:setFixedRotation(true)
-
-    niveau = creerNiveaux("niveaux/niveau_1.txt")
-
-    tailleBlock = 50
-    blocks = {}
-
-    for y, v in ipairs(niveau) do
-        for x, w in ipairs(v) do
-            if w == 1 then
-                table.insert(blocks, creerHitbox(x * tailleBlock, y * tailleBlock, tailleBlock, tailleBlock, "static"))
+    -- niveau
+    local niveau = chargerNiveaux("niveaux/niveau_1.txt")
+    local joueurPositionDebutX = 0
+    local joueurPositionDebutY = 0
+    for y, ligne in ipairs(niveau) do
+        NIVEAU.tailleY = math.max(NIVEAU.tailleY, y)
+        for x, caractere in ipairs(ligne) do
+            if caractere == "x" then
+                table.insert(
+                    NIVEAU.blocs,
+                    creerObjetPhysique(
+                        x * TAILLE_BLOC, y * TAILLE_BLOC,
+                        TAILLE_BLOC, TAILLE_BLOC,
+                        "static"
+                    )
+                )
+            elseif caractere == "j" then
+                joueurPositionDebutX = x * TAILLE_BLOC
+                joueurPositionDebutY = y * TAILLE_BLOC
             end
         end
     end
+
+    -- joueur
+    JOUEUR = creerObjetPhysique(
+        joueurPositionDebutX, joueurPositionDebutY,
+        JOUEUR_TAILLEX, JOUEUR_TAILLEY,
+        "dynamic"
+    )
+    JOUEUR.corps:setFixedRotation(true)
 end
 
 function love.update(dt)
-    -- joueur.surLeSol = false
-    -- local x, y = coordonnees(joueur.hitbox)
-    -- y = y + joueur.h / 2 + 1
-    -- if niveau[]
-
     bougerJoueur(dt)
-    monde:update(dt)
+    MONDE:update(dt)
 end
 
 function bougerJoueur(dt)
-    -- bouger à gauche et à droite
     local vx = 0
+
+    -- bouger à gauche et à droite
     if love.keyboard.isDown("right") then
-        vx = joueur.vitesse
+        vx = vx + JOUEUR_VITESSE
     end
     if love.keyboard.isDown("left") then
-        vx = -joueur.vitesse
+        vx = vx - JOUEUR_VITESSE
     end
-    local _, vy = joueur.hitbox.corps:getLinearVelocity()
-    
+
     -- saut
+    local _, vy = JOUEUR.corps:getLinearVelocity()
     if love.keyboard.isDown("space") then
-        local vx, _ = joueur.hitbox.corps:getLinearVelocity()
-        joueur.hitbox.corps:setLinearVelocity(vx, joueur.forceSaut)
+        -- TODO seulement quand le JOUEUR est sur le sol
+        vy = JOUEUR_VITESSE_SAUT
     end
-    joueur.hitbox.corps:setLinearVelocity(vx, vy)
+
+    -- vitesse de chute maximale
+    vy = math.min(math.max(vy, -JOUEUR_VITESSE_CHUTE_MAX), JOUEUR_VITESSE_CHUTE_MAX)
+    JOUEUR.corps:setLinearVelocity(vx, vy)
+
+    -- quand le joueur tombe, on le fait remonter
+    local maxY = (NIVEAU.tailleY + 1) * TAILLE_BLOC
+    while JOUEUR.corps:getY() > maxY do
+        JOUEUR.corps:setY(JOUEUR.corps:getY() - maxY)
+    end
+
+    -- print(JOUEUR.corps:getY())
 end
 
 function love.draw()
-    love.graphics.draw(joueur.image, joueur.hitbox.corps:getX() - joueur.longueur / 2, joueur.hitbox.corps:getY() - joueur.hauteur / 2, 0,
-                        joueur.longueur / joueur.image:getWidth(),
-                        joueur.hauteur / joueur.image:getHeight())
+    dessinerJoueur()
+    dessinerNiveau()
+end
 
-    for i,v in ipairs(blocks) do
-        love.graphics.draw(joueur.image, v.corps:getX() - tailleBlock / 2, v.corps:getY() - tailleBlock / 2)
+function dessinerJoueur()
+    love.graphics.draw(
+        IMAGES.joueur,
+        JOUEUR.corps:getX() - JOUEUR_TAILLEX / 2,
+        JOUEUR.corps:getY() - JOUEUR_TAILLEY / 2,
+        0, -- orientation
+        JOUEUR_TAILLEX / IMAGES.joueur:getWidth(), -- scaleX
+        JOUEUR_TAILLEY / IMAGES.joueur:getHeight() -- scaleY
+    )
+    -- TODO for some strange reason, player sometimes gets stuck on the ground
+    print(JOUEUR.corps:getY())
+end
+
+function dessinerNiveau()
+    for b = 1, #NIVEAU.blocs do
+        love.graphics.draw(
+            IMAGES.bloc,
+            NIVEAU.blocs[b].corps:getX() - TAILLE_BLOC / 2,
+            NIVEAU.blocs[b].corps:getY() - TAILLE_BLOC / 2
+        )
     end
 end
 
 function love.keypressed(touche)
     if touche == "escape" then
         love.event.quit()
-    elseif touche == "space" then
-        local vx, _ = joueur.hitbox.corps:getLinearVelocity()
-        joueur.hitbox.corps:setLinearVelocity(vx, joueur.forceSaut)
     end
 end
 
@@ -87,31 +134,25 @@ function coordonnees(hitbox)
     return hitbox.corps:getX(), hitbox.corps:getY()
 end
 
-function creerHitbox(x, y, l, h, type, data)
-    local hitbox = {
-        l = l,
-        h = h,
-        corps = love.physics.newBody(monde, x, y, type),
-        forme = love.physics.newRectangleShape(l, h)
+function creerObjetPhysique(x, y, tailleX, tailleY, type)
+    local objet = {
+        corps = love.physics.newBody(MONDE, x, y, type),
+        forme = love.physics.newRectangleShape(tailleX, tailleY)
     }
-    hitbox.fixture = love.physics.newFixture(hitbox.corps, hitbox.forme, 1)
-    hitbox.fixture:setUserData(data)
-
-    return hitbox
+    objet.fixture = love.physics.newFixture(objet.corps, objet.forme)
+    objet.fixture:setFriction(0)
+    return objet
 end
 
-function creerNiveaux(path)
+function chargerNiveaux(path)
     local niveau = {}
     local y = 1
-    for line in io.lines(path) do
+    for line in love.filesystem.lines(path) do
         niveau[y] = {}
-        for x = 1, #line do
-            local s = line:sub(x, x)
-            if s == "x" then
-                niveau[y][x] = 1
-            else
-                niveau[y][x] = 0
-            end
+        local x = 1
+        for s in line:gmatch(".") do
+            niveau[y][x] = s
+            x = x + 1
         end
         y = y + 1
     end
